f115782acc1e3a6be4541852ced691bc437d2b93
Mercy <maritimmercy8@gmail.com>
2025-04-23 12:20:22 +0300

    added Booking var

--- src/MyBookings.jsx
+++ src/MyBookings.jsx
@@ -1,1 +1,6 @@
 import EventList from "./EventList";
+
+const MyBookings = ({ events,MyBookings,onSelect}) => {
+    const bookedEventIds = MyBookings.map(b => b.eventId);
+    const myEvents = events.filter(e => bookedEventIds.includes(e.id));
+}
