609164e9119aeb07a3257ba5d357bcb589d10dfa
Mercy <maritimmercy8@gmail.com>
2025-04-23 12:27:30 +0300

    added div,p

--- src/MyBookings.jsx
+++ src/MyBookings.jsx
@@ -4,8 +4,17 @@ const MyBookings = ({ events,MyBookings,onSelect}) => {
     const bookedEventIds = MyBookings.map(b => b.eventId);
     const myEvents = events.filter(e => bookedEventIds.includes(e.id));
 }
-
-
-
+return (
+    <div>
+    <h2>My Booked Events</h2>
+    <h2>My Booked Events</h2>
+        {myEvents.length > 0 ? (
+          <EventList events={myEvents} onSelect={onSelect} />
+        ) : (
+          <p>You haven't booked any events yet.</p>
+        )}
+      </div>
+    );
+  
 
 export default MyBookings;
