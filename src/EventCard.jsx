396aff96bd2335cd4cc6632db7777be80800f054
Michael-Ngochi <ngoochimike@gmail.com>
2025-04-24 04:46:54 +0300

    added conditional rendering to the event details

--- src/EventCard.jsx
+++ src/EventCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 const EventCard = ({ event }) => {
+
+
     return (
       <Link to={`/events/${event.id}`} className="event-card-link">
       <div className="event-card">
@@ -8,7 +10,7 @@ const EventCard = ({ event }) => {
         <div className="event-info">
           <h3>{event.title}</h3>
           <p>{event.date} @ {event.location}</p>
-          <p>KES {event.price} per ticket</p>
+          {event.tickets!==0?<p>KES {event.price} per ticket</p>:<p style={{background:"red",width:"fit-content",padding:"5px",borderRadius:"10px",fontSize:"large"}}><strong>SOLD OUT</strong></p>}
         </div>
       </div>
       </Link>
